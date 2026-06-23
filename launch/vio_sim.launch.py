99cc6ea4aff3732745b33181b8f85e91bc82f400
taanviarora3110 <taanviarora3110@gmail.com>
2026-06-23 13:42:58 +0530

    Add VIO simulation launch file and progress documentation

---
+++ launch/vio_sim.launch.py
@@ -0,0 +1,23 @@
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+
+    return LaunchDescription([
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                "/ws/ros_ws/src/tello/tello_driver/launch/sim.launch.py"
+            )
+        ),
+
+        ExecuteProcess(
+            cmd=[
+                "/ws/ros_ws/build/slam/basalt_slam_node",
+                "--ros-args",
+                "-p", "camera_topic_name:=/drone1/image_raw"
+            ],
+            output="screen"
+        )
+    ])
